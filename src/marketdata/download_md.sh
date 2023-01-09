d72636ebd524c39bdfc91b1a121ef4de16456d3b
Stanislav U <SimSonic@users.noreply.github.com>
2023-01-09 22:57:06 +0700

    Исправлено падение download_md.sh при повторе после статуса 429

--- src/marketdata/download_md.sh
+++ src/marketdata/download_md.sh
@@ -13,22 +13,23 @@ function download {
     ((year--))
     download "$figi" "$year";
   fi
-#Если превышен лимит запросов в минуту (30) - повторяем запрос
+  # Если превышен лимит запросов в минуту (30) - повторяем запрос.
   if [ "$response_code" = "429" ]; then
       echo "rate limit exceed. sleep 5"
       sleep 5
       download "$figi" "$year";
+      return 0
   fi
-#Если невалидный токен - выходим
+  # Если невалидный токен - выходим.
   if [ "$response_code" = "401" ] || [ "$response_code" = "500" ]; then
       echo 'invalid token'
       exit 1
   fi
-
+  # Если данные по инструменту за указанный год не найдены.
   if [ "$response_code" = "404" ]; then
       echo "data not found for figi=${figi}, year=${year}, skipped"
   elif [ "$response_code" != "200" ]; then
-      #В случае другой ошибки - просто напишем ее в консоль и выйдем
+      # В случае другой ошибки - просто напишем ее в консоль и выйдем.
       echo "unspecified error with code: ${response_code}"
       exit 1
   fi
